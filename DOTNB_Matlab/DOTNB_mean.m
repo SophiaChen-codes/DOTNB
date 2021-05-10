d4a89a6c7f4e0788239f5f270a1e5d55b2b1cc31
chenyongrowan <83730164+chenyongrowan@users.noreply.github.com>
2021-05-10 10:39:53 -0400

    Add files via upload

---
+++ DOTNB_Matlab/DOTNB_mean.m
@@ -0,0 +1,10 @@
+function dmean=DOTNB_mean(r1,p1,r2,p2)
+
+% calculate the mean of the difference of two NB distributions (r1,p1) and (r2,p2)
+q1 = 1-p1; q2 = 1-p2;
+
+dmean = vpa(r1*q1/p1 - r2*q2/p2);
+
+end
+
+
